21ab713d870e563c5b90106feaa01762886aad2b
alewishes9 <68523887+alewishes9@users.noreply.github.com>
2020-09-09 23:23:33 -0400

    AR_SoundVis
    AR_SoundVis

---
+++ Assets/Script/RaycastIntersection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class RaycastIntersection : MonoBehaviour
+{
+    private ARRaycastManager aRRaycastManager;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private Transform hitTransform;
+
+    public Transform intersectionTransform
+    {
+        get { return hitTransform; }
+        
+    }
+
+    public bool TryGetTouchPosition()
+    {
+        if (Input.touchCount > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void Awake()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aRRaycastManager.Raycast(Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2)), hits, TrackableType.PlaneWithinPolygon))
+        {
+            Pose hitPose = hits[0].pose;
+            hitTransform.position = hitPose.position;
+            hitTransform.rotation = hitPose.rotation;
+
+        }
+    }
+}
---
+++ Assets/Script/TimedGetIntersection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedGetIntersection : MonoBehaviour
+{
+    public RaycastIntersection raycastIntersection;
+
+    public float seconds = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TimedIntersection());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator TimedIntersection()
+    {
+        yield return new WaitForSeconds(seconds);
+        Transform intersection = raycastIntersection.intersectionTransform;
+        
+
+    }
+}
